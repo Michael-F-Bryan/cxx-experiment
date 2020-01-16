1011caa8e049adc70bcd8f42299c0f27a0b7ff48
Michael Bryan <michaelfbryan@gmail.com>
2020-01-16 22:18:07 +0800

    Tried to build and link libtorrent

--- build.rs
+++ build.rs
@@ -1,5 +1,29 @@
-use std::{env, path::PathBuf};
+use cmake::Config;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn main() {
-    let _project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let vendor = project_dir.join("vendor");
+
+    let libtorrent = vendor.join("libtorrent");
+    let dst = Config::new(&libtorrent)
+        .define("BUILD_SHARED_LIBS", "FALSE")
+        .define("static_runtime", "TRUE")
+        .uses_cxx11()
+        .build();
+
+    println!("cargo:warning=Output directory: {}", dst.display());
+    add_to_search_path(&dst);
+    println!("cargo:rustc-link-lib=static=torrent-rasterbar");
+}
+
+fn add_to_search_path(path: &Path) {
+    println!("cargo:rustc-link-search=native={}", path.display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        path.join("build").display()
+    );
 }
